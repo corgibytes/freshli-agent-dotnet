caba828f653c5f5d91879612e8e96a3d02cd0974
Mike Dougherty <mike@corgibytes.com>
2023-05-09 13:29:27 -0400

    implement validating-package-urls command
    Relates to issue #1

--- Corgibytes.Freshli.Agent.DotNet/Commands/ValidatingPackageUrls.cs
+++ Corgibytes.Freshli.Agent.DotNet/Commands/ValidatingPackageUrls.cs
@@ -1,12 +1,29 @@
 using System.CommandLine;
+using System.CommandLine.NamingConventionBinder;
 
 namespace Corgibytes.Freshli.Agent.DotNet.Commands;
 
 public class ValidatingPackageUrls : Command
 {
+    readonly List<string> _packageUrls = new()
+    {
+        "pkg:nuget/org.nuget/packages/Corgibytes.Freshli.Lib",
+        "pkg:nuget/org.nuget/packages/System.CommandLine",
+        "pkg:nuget/org.nuget/packages/Microsoft.CSharp",
+    };
+
     public ValidatingPackageUrls() :
         base("validating-package-urls",
             "Lists package urls that can be used to validate this agent")
     {
+        Handler = CommandHandler.Create(Run);
+    }
+
+    public void Run()
+    {
+        foreach (string packageUrl in _packageUrls)
+        {
+            Console.WriteLine(packageUrl);
+        }
     }
 }
