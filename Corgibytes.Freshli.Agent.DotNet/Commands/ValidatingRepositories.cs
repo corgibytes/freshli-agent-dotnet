69d85e8a6d9acbec433c712cdcd0610fe2392cb9
Mike Dougherty <mike@corgibytes.com>
2023-05-09 14:53:09 -0400

    implement validating-repositories command
    Relates to issue #1

--- Corgibytes.Freshli.Agent.DotNet/Commands/ValidatingRepositories.cs
+++ Corgibytes.Freshli.Agent.DotNet/Commands/ValidatingRepositories.cs
@@ -1,11 +1,30 @@
 using System.CommandLine;
+using System.CommandLine.NamingConventionBinder;
 
 namespace Corgibytes.Freshli.Agent.DotNet.Commands;
 
 public class ValidatingRepositories : Command
 {
-    public ValidatingRepositories() :
-        base("validating-repositories", "Lists repositories that can be used to validate this agent")
+    readonly List<string> _repositoryUrls = new()
     {
+        "https://github.com/dotnet/docs",
+        "https://github.com/questdb/questdb",
+        "https://github.com/protocolbuffers/protobuf",
+        "https://github.com/serverless/serverless"
+    };
+
+    public ValidatingRepositories() :
+        base("validating-repositories",
+            "Lists repositories that can be used to validate this agent")
+    {
+        Handler = CommandHandler.Create(Run);
+    }
+
+    public void Run()
+    {
+        foreach (string repositoryUrl in _repositoryUrls)
+        {
+            Console.WriteLine(repositoryUrl);
+        }
     }
 }
