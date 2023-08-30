d1bdbc182d9c82ad65d653db9c6c0dee4309d0a7
M. Scott Ford <scott@corgibytes.com>
2023-08-30 14:51:20 -0400

    Cleans up formatting

--- Corgibytes.Freshli.Agent.DotNet/Lib/ValidatingData.cs
+++ Corgibytes.Freshli.Agent.DotNet/Lib/ValidatingData.cs
@@ -2,19 +2,15 @@ namespace Corgibytes.Freshli.Agent.DotNet.Lib;
 
 public static class ValidatingData
 {
-    static readonly List<string> s_packageUrls = new()
+    public static List<string> PackageUrls() => new()
     {
         "pkg:nuget/Corgibytes.Freshli.Lib@0.5.0",
         "pkg:nuget/System.CommandLine@2.0.0-beta4.22272.1",
         "pkg:nuget/Microsoft.CSharp@4.7.0",
     };
 
-    static readonly List<string> s_repositoryUrls = new()
+    public static List<string> RepositoryUrls() => new()
     {
         "https://github.com/corgibytes/freshli-fixture-csharp-test",
     };
-
-    public static List<string> PackageUrls() => s_packageUrls;
-
-    public static List<string> RepositoryUrls() => s_repositoryUrls;
 }
