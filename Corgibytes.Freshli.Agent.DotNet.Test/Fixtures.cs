a988cc1a89add679b0b018a7ac44c29f5f7ac430
M. Scott Ford <scott@corgibytes.com>
2023-09-07 11:58:20 -0400

    Returns path to an isolated fixture file
    Fixture file modifications by some of the tests are causing them to sometimes fail depending on the order that the tests are run in.
    
    This copies the entire fixtures directory tree to a temp location, and then returns a path to the newly created temp file version of the requested file.

--- Corgibytes.Freshli.Agent.DotNet.Test/Fixtures.cs
+++ Corgibytes.Freshli.Agent.DotNet.Test/Fixtures.cs
@@ -1,4 +1,5 @@
 using System.Reflection;
+using IOPath = System.IO.Path;
 
 namespace Corgibytes.Freshli.Agent.DotNet.Test;
 
@@ -6,23 +7,62 @@ public static class Fixtures
 {
     public static string Path(params string[] values)
     {
+        var pristineFixturesPath = GetPristineFixturesPath();
+        var components = new List<string> { pristineFixturesPath };
+        components.AddRange(values);
+
+        var pristineResult = IOPath.Combine(components.ToArray());
+
+        if (!Directory.Exists(pristineResult) && !File.Exists(pristineResult))
+        {
+            throw new FixtureNotFoundException(
+                "The specified file or directory was not found within the `Fixtures` directory tree", pristineResult);
+        }
+
+        // create temp directory and then copy the fixture to it
+        var tempDir = IOPath.Combine(IOPath.GetTempPath(), Guid.NewGuid().ToString());
+        Directory.CreateDirectory(tempDir);
+
+        CopyFixturesToTemp(tempDir);
+
+        return pristineResult.Replace(pristineFixturesPath, tempDir);
+    }
+
+    private static string GetPristineFixturesPath()
+    {
         var assemblyPath = Assembly.GetExecutingAssembly().Location;
         var components = new List<string>()
         {
             Directory.GetParent(assemblyPath)!.ToString(),
             "Fixtures"
         };
-        components.AddRange(values);
+        return IOPath.Combine(components.ToArray());
+    }
 
-        var result = System.IO.Path.Combine(components.ToArray());
+    private static void CopyFixturesToTemp(string tempLocation)
+    {
+        var pristineFixtures = GetPristineFixturesPath();
 
-        if (!Directory.Exists(result) && !File.Exists(result))
+        var directoriesToProcess = new HashSet<string> { pristineFixtures };
+
+        while (directoriesToProcess.Count > 0)
         {
-            throw new FixtureNotFoundException(
-                "The specified file or directory was not found within the `Fixtures` directory tree", result);
-        }
+            var currentDir = directoriesToProcess.First();
+            directoriesToProcess.Remove(currentDir);
 
-        return result;
+            foreach (var file in Directory.GetFiles(currentDir))
+            {
+                var tempFilePath = file.Replace(pristineFixtures, tempLocation);
+                File.Copy(file, tempFilePath);
+            }
+
+            foreach (var directory in Directory.GetDirectories(currentDir))
+            {
+                var targetDir = directory.Replace(pristineFixtures, tempLocation);
+                Directory.CreateDirectory(targetDir);
+                directoriesToProcess.Add(directory);
+            }
+        }
     }
 
     private class FixtureNotFoundException : FileNotFoundException
