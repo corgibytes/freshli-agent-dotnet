033ccd9d553eaf0809a47ec76edcd90f71d058b1
M. Scott Ford <scott@corgibytes.com>
2023-09-08 11:41:18 -0400

    Removes commands other than `start-server`
    The other commands are not needed, because the Freshli CLI only interacts with the language agent via gRPC calls.

--- Corgibytes.Freshli.Agent.DotNet/Commands/MainCommand.cs
+++ Corgibytes.Freshli.Agent.DotNet/Commands/MainCommand.cs
@@ -7,11 +7,6 @@ public class MainCommand : RootCommand
 {
     public MainCommand(string description = "") : base(description)
     {
-        Add(new ValidatingPackageUrls());
-        Add(new RetrieveReleaseHistory());
-        Add(new ValidatingRepositories());
-        Add(new DetectManifests());
-        Add(new ProcessManifest());
         Add(new StartServer());
     }
 }
